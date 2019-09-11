1dc4eaaa4f60339591dba6268c9cbfbf241b41b6
David Petringa <david@ajaxtransport.com>
2019-09-11 10:47:57 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Book Loads Online</title>
+
+		<meta charset="UTF-8">
+		<meta name="description" content="Carriers book loads Online, Anytime, Anywhere. Trucking companies search for loads and keep your trucks loaded.">
+		<meta name="keywords" content="book loads online, search for loads, trucking, freight logistics, trucking companies, freight, brokers, logistics, carriers, ajax transport">
+		<meta name="author" content="David Petringa, Coded April 2018">
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+		<link href = "http://www.dukesnuz.com/css_libs/dukes_normalize.css" rel = "stylesheet">
+		<link href="styles.css?m=<?php echo rand(); ?>" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+
+		<header>
+			<h1>Book Loads Online</h1>
+			<h2>Find Freight For Your Trucks</h2>
+		</header>
+
+		<section class="content">
+			<h3>Trucking Companies Book Loads</h3>
+			<div class="register">
+				<p>
+					<a href="http://www.ajaxtransport.com/carrier/register" target="blank">Register Now</a>
+				</p>
+			</div>
+			<ul>
+				<li>
+					High freight rates
+				</li>
+				<li>
+					Carriers search for loads
+				</li>
+				<li>
+					Book loads online anytime anywhere
+				</li>
+				<li>
+					Get paid extra for booking online
+				</li>
+				<li>
+					Keep your trucks loaded and rolling
+				</li>
+				<li>
+					Minimize your empty miles
+				</li>
+				<li>
+					Online, Anytime, Anywhere
+				</li>
+			</ul>
+		</section>
+
+		<footer>
+			<h5>Bookloadsonline.com is part of Ajax Trans. Co., Inc. A freight brokerage company.</h5>
+			<ul>
+				<li>
+					<a href="http://www.ajaxtransport.com" target="blank">Online freight brokerage at Ajax Transport</a>
+				</li>
+				<li>
+					<a href="http://www.thecentraldispatch.com" target="blank">Valuable transportation links at The Central Dispatch</a>
+				</li>
+				<li>
+					<a href="http://www.loadedandrolling.co" target="blank">Carrier look up at Loaded and Rolling .co</a>
+				</li>
+				<li>
+					<a href="http://www.loadedandrolling.com" target="blank">Load posting board Loaded and Rolling .com</a>
+				</li>
+				<li>
+					<a href="http://www.ajaxforums.com" target="blank">Ajax Forums</a>
+				</li>
+			</ul>
+			<p>
+				Copyright &copy; <?php echo date("Y"); ?>
+				bookloadsonline.com. All rights reserved.
+			</p>
+			<p>
+				Designed and developed by: <a href="http://www.dukesnuz.com" target="blank">David Petringa</a>
+			</p>
+		</footer>
+		<!-- Default Statcounter code for Bookloadsonline
+		http://www.bookloadsonline.com -->
+		<script type="text/javascript">
+			var sc_project = 8826867;
+			var sc_invisible = 1;
+			var sc_security = "6fc9c86e";
+			var scJsHost = (("https:" == document.location.protocol) ? "https://secure." : "http://www.");
+			document.write("<sc" + "ript type='text/javascript' src='" + scJsHost + "statcounter.com/counter/counter.js'></" + "script>");
+		</script>
+		<noscript>
+			<div class="statcounter">
+				<a title="site stats"
+				href="http://statcounter.com/" target="_blank"><img
+				class="statcounter"
+				src="//c.statcounter.com/8826867/0/6fc9c86e/0/" alt="site
+				stats"></a>
+			</div>
+		</noscript>
+		<!-- End of Statcounter Code -->
+	</body>
+</html>
